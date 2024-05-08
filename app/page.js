8aae8ff3075f9c55f78ed6d3893e558f83f46726
Shijoy Bharath <shijoybharath8@gmail.com>
2024-05-08 09:08:20 +0530

    Working widget with tailwind classes

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Script from "next/script";
 export default function Home() {
   return (
     <div>
-      <div data-symbol="SYMBOL FROM THE EMBED DIV" class="roadmap-widget"></div>
+      <div data-symbol="SYMBOL FROM THE EMBED DIV" className="roadmap-widget"></div>
       <Script src="roadmap.js"></Script>
       <roadmap/>
     </div>
